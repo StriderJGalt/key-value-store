32d59f0e192ca6d60e86fa7de22757cd60bee2fc
SambhavSolanki <sambhav.solanki@research.iiit.ac.in>
2020-02-16 23:54:03 +0530

    Made node to a class

--- kvStore.cpp
+++ kvStore.cpp
@@ -5,20 +5,26 @@ struct Slice{
         uint8_t size;
         char* data;
 };
-struct node
+class node
 {
+public:
        Slice *key;
-       key->size = 64;
-       key->data = malloc(sizeof(char)*(key->size));
        Slice *value;
-       value->size = 256;
-       value->data = malloc(sizeof(char)*(value->size));
        node *parent;
        char color;
        node *left;
        node *right;
        int lsize;
        int rsize;
+public:
+       node(){
+        key->size = 63;
+        key->data = (char *)malloc(sizeof(char)*(key->size));
+        value->size = 255;
+        value->data = (char *)malloc(sizeof(char)*(value->size));
+        lsize = 0;
+        rsize = 0;
+      }
 };
 
 class kvstore{
