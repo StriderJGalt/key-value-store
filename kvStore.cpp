adacb166058f07686d29e33e9e736a5e2c919540
SambhavSolanki <sambhav.solanki@research.iiit.ac.in>
2020-02-17 00:23:05 +0530

    del initialise

--- kvStore.cpp
+++ kvStore.cpp
@@ -43,7 +43,82 @@ public:
 	}
 
 	bool del(string key){
-		//Your Code Here
+    node *p;
+    p=root;
+    node *y=NULL;
+    node *q=NULL;
+    int found=0;
+    while(p!=NULL&&found==0)
+    {
+          if(p->key->data==x)##
+              found=1;
+          if(found==0)
+          {
+                if(p->key->data<x)##
+                   p=p->right;
+                else
+                   p=p->left;
+          }
+    }
+    if(found==0)
+    {
+           cout<<"\nElement Not Found.";
+           return ;
+    }
+    else
+    {
+        cout<<"\nDeleted Element: "<<p->key;
+        cout<<"\nColour: ";
+        if(p->color=='b')
+    cout<<"Black\n";
+   else
+    cout<<"Red\n";
+
+        if(p->parent!=NULL)
+              cout<<"\nParent: "<<p->parent->key;
+        else
+              cout<<"\nThere is no parent of the node.  ";
+        if(p->right!=NULL)
+              cout<<"\nRight Child: "<<p->right->key;
+        else
+              cout<<"\nThere is no right child of the node.  ";
+        if(p->left!=NULL)
+              cout<<"\nLeft Child: "<<p->left->key;
+        else
+              cout<<"\nThere is no left child of the node.  ";
+        cout<<"\nNode Deleted.";
+        if(p->left==NULL||p->right==NULL)
+             y=p;
+        else
+             y=successor(p);
+        if(y->left!=NULL)
+             q=y->left;
+        else
+        {
+             if(y->right!=NULL)
+                  q=y->right;
+             else
+                  q=NULL;
+        }
+        if(q!=NULL)
+             q->parent=y->parent;
+        if(y->parent==NULL)
+             root=q;
+        else
+        {
+            if(y==y->parent->left)
+               y->parent->left=q;
+            else
+               y->parent->right=q;
+        }
+        if(y!=p)
+        {
+            p->color=y->color;
+            p->key=y->key;
+        }
+        if(y->color=='b')
+            delfix(q);
+    }
 		return false;
 	}
 
